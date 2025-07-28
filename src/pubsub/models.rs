dbffd110cf88166a3edbbc8e0d089b8f6b5a4885
neoscript <neoscripting@gmail.com>
2025-07-27 22:03:52 -0500

    feat: create types for sending pubsub message
    note: need to see if there is a simpler way to garuntee the base64 conversion

--- src/pubsub/models.rs
+++ src/pubsub/models.rs
@@ -1,5 +1,7 @@
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use base64::{Engine as _, engine::general_purpose::STANDARD};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Deserializer, Serialize};
+use std::{collections::HashMap, str::Bytes};
 
 /// The struct fields are not comprehensive
 /// if you need a certain field that is not included, send a PR
@@ -12,4 +14,45 @@ pub struct Topic {
 #[derive(Deserialize, Debug)]
 pub struct ListTopicsResponse {
     pub topics: Vec<Topic>,
+}
+
+#[derive(Serialize, Debug)]
+pub struct PublishMessagePayload {
+    messages: Vec<SendablePubsubMessage>,
+}
+
+#[derive(Debug)]
+pub struct RawPubsubMessageToSend {
+    // NOTE: this data payload need to be encoded to b64 before sending out
+    data: Option<String>,
+    attributes: Option<HashMap<String, String>>,
+    ordering_key: Option<String>,
+}
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SendablePubsubMessage {
+    data: Option<String>,
+    attributes: Option<HashMap<String, String>>,
+    ordering_key: Option<String>,
+}
+
+impl From<RawPubsubMessageToSend> for SendablePubsubMessage {
+    fn from(raw: RawPubsubMessageToSend) -> Self {
+        Self {
+            data: raw.data.map(|payload| STANDARD.encode(payload)),
+            attributes: raw.attributes,
+            ordering_key: raw.ordering_key,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PubsubMessageRecieved {
+    data: Option<String>,
+    attributes: Option<HashMap<String, String>>,
+    message_id: String,
+    publish_time: DateTime<Utc>,
+    ordering_key: Option<String>,
 }
