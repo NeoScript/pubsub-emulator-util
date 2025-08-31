9ca643804d458cb13d61cfee0c005ad4a2b6486e
neoscript <neoscripting@gmail.com>
2025-08-31 00:36:32 -0500

    make multiple topics

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod pubsub;
 #[tokio::main]
 async fn main() {
     let client = Client::new();
-    const PROJECT_ID: &str = "my-local-project";
+    const PROJECT_ID: &str = "my-project";
     const HOST: &str = "http://localhost:8085";
     let topics = topics::list(PROJECT_ID, &client, HOST).await;
 
@@ -28,12 +28,15 @@ async fn main() {
         Err(ref e) => println!("Error pulling topics: {e}"),
     }
 
-    let topic_id = Alphanumeric.sample_string(&mut rand::rng(), 8);
-    let topic = topics::create(PROJECT_ID, &client, HOST, &topic_id)
-        .await
-        .expect("should create topic");
-    println!("topic result: {topic:?}");
+    for _ in 0..2 {
+        let topic_id = Alphanumeric.sample_string(&mut rand::rng(), 8);
+        let topic = topics::create(PROJECT_ID, &client, HOST, &topic_id)
+            .await
+            .expect("should create topic");
+        println!("topic created: {topic:?}");
+    }
 
+    let topics = topics::list(PROJECT_ID, &client, HOST).await;
     let binding = topics.unwrap();
     let delete_topic = binding.topics.choose(&mut rand::rng()).unwrap();
     let delete_repsonse = topics::delete(&client, HOST, &delete_topic.name).await;
