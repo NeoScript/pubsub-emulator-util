40799401b31c1640d3199edb6ce63bf43b4c85f0
neoscript <neoscripting@gmail.com>
2025-09-05 14:37:49 -0500

    wip: trying to get basic logic written out

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
@@ -5,18 +7,44 @@ use clap::{Args, Parser, Subcommand};
 pub struct Cli {
     #[command(subcommand)]
     pub commands: PubsubCommands,
-
-    #[arg(short, long)]
-    pub project_id: String,
-
-    #[arg(long, env)]
-    pub host: String,
+    // #[arg(long, env)]
+    // pub host: String,
 }
 
 #[derive(Subcommand)]
 pub enum PubsubCommands {
     #[command(subcommand)]
     Topics(TopicCommands),
+
+    #[command()]
+    Init(InitArgs),
+}
+
+#[derive(Parser, Debug)]
+pub struct InitArgs {
+    #[arg(
+        short,
+        long,
+        help = "path to file containing initial state",
+        value_name = "FILE_PATH"
+    )]
+    pub file: String,
+
+    #[arg(
+        long,
+        env("PUBSUB_EMULATOR_HOST"),
+        help = "the pubsub emulator host in the following format: `http://addr:port`"
+    )]
+    pub host: String,
+
+    #[arg(
+        short,
+        long,
+        value_name = "SECONDS",
+        value_parser = clap::value_parser!(u8).range(0..=255),
+        help = "how long (in seconds) to poll the pubsub emulator host before giving up."
+    )]
+    pub timeout: Option<u8>,
 }
 
 #[derive(Subcommand)]
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use cli::Cli;
 use reqwest::Client;
 
 mod cli;
+mod parser;
 mod pubsub;
 
-use crate::cli::{PubsubCommands, TopicCommands};
+use crate::cli::{InitArgs, PubsubCommands, TopicCommands};
 
 struct Config {
     client: Arc<Client>,
@@ -22,18 +23,31 @@ async fn main() {
     let client = Client::new();
     let config = Config {
         client: Arc::new(client),
-        project_id: cli.project_id.to_string(),
-        host: cli.host.to_string(),
+        project_id: "my-project".to_string(), // TODO: fix this again
+        host: "http://localhost:8090".to_string(), //TODO: fix this too
     };
 
     let result = match &cli.commands {
         PubsubCommands::Topics(cmd) => handle_topics(cmd, &config).await,
+        PubsubCommands::Init(init_args) => handle_init(init_args, &config).await,
     };
 
     match result {
         Ok(_) => println!("Completed Successfully"),
         Err(e) => eprintln!("Error occured: {}", e),
     }
+}
+
+async fn handle_init(args: &InitArgs, config: &Config) -> Result<(), reqwest::Error> {
+    println!("{:?}", args);
+    let config = parser::parse_init_file(&args.file);
+
+    println!("config: {:?}", &config);
+    // NOTE: need to do the following
+    // poll the host for a connection
+    // then attempt to create the topics/subscriptions for the given project in async
+    // print out any success/failures
+    Ok(())
 }
 
 async fn handle_topics(cmd: &TopicCommands, config: &Config) -> Result<(), reqwest::Error> {
