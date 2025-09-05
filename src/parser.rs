12c48cce3f21855bbb119b6c45463d502f0780cf
neoscript <neoscripting@gmail.com>
2025-09-05 14:27:35 -0500

    feat: Update options to make init command work
    need to rethink approach to other vars for other commands

--- src/parser.rs
+++ src/parser.rs
@@ -1,15 +1,30 @@
+use std::{fs::File, io, path::Path};
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
 pub struct ProjectInitFile {
-    project_id: String,
-    topics: Vec<TopicInitEntry>,
+    pub project_id: String,
+    pub topics: Vec<TopicInitEntry>,
 }
 
+#[derive(Deserialize, Debug)]
 pub struct PushSubInitEntry {
-    name: String,
-    endpoint: String,
+    pub name: String,
+    pub endpoint: String,
 }
 
+#[derive(Deserialize, Debug)]
 pub struct TopicInitEntry {
-    topic: String,
-    pull_subscriptions: Vec<String>,
-    push_subscriptions: Vec<PushSubInitEntry>,
+    pub name: String,
+    pub pull_subscriptions: Option<Vec<String>>,
+    pub push_subscriptions: Option<Vec<PushSubInitEntry>>,
+}
+
+pub fn parse_init_file<P: AsRef<Path>>(path: P) -> Result<ProjectInitFile, io::Error> {
+    let path = path.as_ref();
+    let file = File::open(path)?;
+
+    let init_config = serde_json::from_reader(file)?;
+    Ok(init_config)
 }
